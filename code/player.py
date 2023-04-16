87445fbbef0ab5cafc667331c9ffa34a1c68b4ed
Thisgo1 <thisgo.014@gmail.com>
2023-04-16 15:55:56 -0300

    💄 ✨ Adiciona maçãs e corte das arvores.

--- code/player.py
+++ code/player.py
@@ -6,7 +6,7 @@ from timer import Timer
 
 class Player(pygame.sprite.Sprite):
 
-    def __init__(self, pos, group, collision_sprites):
+    def __init__(self, pos, group, collision_sprites, tree_sprites):
         super().__init__(group)
 
         self.import_assets()
@@ -46,8 +46,23 @@ class Player(pygame.sprite.Sprite):
         self.seed_index = 0
         self.selected_seed = self.seeds[self.seed_index]
 
+        # interaction
+        self.tree_sprites = tree_sprites
+
     def use_tool(self):
-        pass
+        if self.selected_tool == 'axe':
+            for tree in self.tree_sprites.sprites():
+                if tree.rect.collidepoint(self.target_pos):
+                    tree.damage()
+        if self.selected_tool == 'hoe':
+            pass
+        if self.selected_tool == 'water':
+            pass
+
+    def get_target_position(self):
+
+        self.target_pos = self.rect.center + \
+            PLAYER_TOOL_OFFSET[self.status.split('_')[0]]
 
     def use_seed(self):
         pass
@@ -164,24 +179,24 @@ class Player(pygame.sprite.Sprite):
             if hasattr(sprite, 'hitbox'):
                 if sprite.hitbox.colliderect(self.hitbox):
                     if direction == 'horizontal':
-                        if self.direction.x > 0: #indo para direita
+                        if self.direction.x > 0:  # indo para direita
                             self.hitbox.right = sprite.hitbox.left
-                        if self.direction.x < 0: #indo para esquerda
+                        if self.direction.x < 0:  # indo para esquerda
                             self.hitbox.left = sprite.hitbox.right
                         self.rect.centerx = self.hitbox.centerx
                         self.pos.x = self.hitbox.centerx
                     if direction == 'vertical':
-                        if self.direction.y > 0: #indo para baixo
+                        if self.direction.y > 0:  # indo para baixo
                             self.hitbox.bottom = sprite.hitbox.top
-                        if self.direction.y < 0: #indo para cima
+                        if self.direction.y < 0:  # indo para cima
                             self.hitbox.top = sprite.hitbox.bottom
                         self.rect.centery = self.hitbox.centery
                         self.pos.y = self.hitbox.centery
-                        
 
     def update(self, dt):
         self.input()
         self.move(dt)
         self.animate(dt)
         self.update_timers()
+        self.get_target_position()
         self.get_status()
--- code/sprites.py
+++ code/sprites.py
@@ -1,5 +1,7 @@
 import pygame
 from settings import *
+from random import randint, choice
+from timer import Timer
 
 
 class Generic(pygame.sprite.Sprite):
@@ -8,7 +10,9 @@ class Generic(pygame.sprite.Sprite):
         self.image = surf
         self.rect = self.image.get_rect(topleft=pos)
         self.z = z
-        self.hitbox = self.rect.copy().inflate(-self.rect.width * 0.2, -self.rect.height * 0.75 )
+        self.hitbox = self.rect.copy().inflate(-self.rect.width *
+                                               0.2, -self.rect.height * 0.75)
+
 
 class Water(Generic):
     def __init__(self, pos, frames, groups):
@@ -32,11 +36,58 @@ class Water(Generic):
     def update(self, dt):
         self.animate(dt)
 
+
 class WildFlower(Generic):
-  def __init__(self, pos, surf, groups):
-      super().__init__(pos, surf, groups)
-      self.hitbox = self.rect.copy().inflate(-20,-self.rect.height * 0.9)
+    def __init__(self, pos, surf, groups):
+        super().__init__(pos, surf, groups)
+        self.hitbox = self.rect.copy().inflate(-20, -self.rect.height * 0.9)
+
 
 class Tree(Generic):
-  def __init__(self, pos, surf, groups, name):
-      super().__init__(pos, surf, groups)
+    def __init__(self, pos, surf, groups, name):
+        super().__init__(pos, surf, groups)
+
+        #atributos da arvores
+        self.health = 5
+        self.alive = True
+        stump_path = f'../graphics/stumps/{"small" if name == "Small" else "large"}.png'
+        self.stump_surf = pygame.image.load(stump_path).convert_alpha()
+        self.invult_timer = Timer(200)
+        
+        # maçãs
+        self.apple_surf = pygame.image.load('../graphics/fruit/apple.png')
+        self.apple_pos = APPLE_POS[name]
+        self.apple_sprites = pygame.sprite.Group()
+        self.create_fruit()
+
+    def create_fruit(self):
+        for pos in self.apple_pos:
+            if randint(0, 10) < 2:
+                x = pos[0] + self.rect.left
+                y = pos[1] + self.rect.top
+                Generic(
+                    pos = (x, y), 
+                    surf = self.apple_surf, 
+                    groups = [self.apple_sprites, self.groups()[0]],
+                    z = LAYERS['fruit'])
+
+    def damage(self):
+        #bater na arvore
+        self.health -=1
+        
+        
+        #dropar maçã
+        if len(self.apple_sprites.sprites()) > 0:
+            random_apple = choice(self.apple_sprites.sprites())
+            random_apple.kill()
+
+    def check_death(self): 
+        if self.health <= 0:
+            self.image = self.stump_surf
+            self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
+            self.hitbox = self.rect.inflate(-10, -self.rect.height*0.6)
+            self.alive = False
+    
+    def update(self, dt):
+        if self.alive:
+            self.check_death()        
